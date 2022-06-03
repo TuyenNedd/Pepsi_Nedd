bc4acb73fbe7e7bc2c48e7f1108fc9f95e8ac290
TuyenNedd <trituyen2003@gmail.com>
2022-06-03 18:47:53 +0700

    update onclick-event changeCan, changeBG

--- js/index.js
+++ js/index.js
@@ -1,25 +1,45 @@
-const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
-const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
-const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
-const header = document.querySelector('.header.container');
+const hamburger = document.querySelector(
+  ".header .nav-bar .nav-list .hamburger"
+);
+const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
+const menu_item = document.querySelectorAll(
+  ".header .nav-bar .nav-list ul li a"
+);
+const header = document.querySelector(".header.container");
 
-hamburger.addEventListener('click', () => {
-	hamburger.classList.toggle('active');
-	mobile_menu.classList.toggle('active');
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  mobile_menu.classList.toggle("active");
 });
 
-document.addEventListener('scroll', () => {
-	var scroll_position = window.scrollY;
-	if (scroll_position > 250) {
-		header.style.backgroundColor = '#0065c3';
-	} else {
-		header.style.backgroundColor = 'transparent';
-	}
+document.addEventListener("scroll", () => {
+  var scroll_position = window.scrollY;
+  if (scroll_position > 250) {
+    header.style.backgroundColor = "#0065c3";
+  } else {
+    header.style.backgroundColor = "transparent";
+  }
 });
 
 menu_item.forEach((item) => {
-	item.addEventListener('click', () => {
-		hamburger.classList.toggle('active');
-		mobile_menu.classList.toggle('active');
-	});
+  item.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    mobile_menu.classList.toggle("active");
+  });
 });
+
+function changeCan(anything) {
+  document.querySelector(".pepsi_can").src = anything;
+}
+function changeBGBlue() {
+  document.getElementById("changeBG").style.backgroundImage =
+    "url(img/bluebg.png)";
+}
+function changeBGRed() {
+  document.getElementById("changeBG").style.backgroundImage =
+    "url(img/whitebg.png)";
+}
+function changeBGBlack() {
+  document.getElementById("changeBG").style.backgroundImage =
+    "url(img/blackbg.png)";
+}
