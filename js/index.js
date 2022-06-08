fdf221ea8ec38f912b15f5f8dcb1e1b79ca309cb
TuyenNedd <trituyen2003@gmail.com>
2022-06-08 16:18:05 +0700

    update cursor_animation swipe_slider

--- js/index.js
+++ js/index.js
@@ -43,3 +43,46 @@ function changeBGBlack() {
   document.getElementById("changeBG").style.backgroundImage =
     "url(img/blackbg.png)";
 }
+
+new Vue({
+  el: "#app",
+  data: {
+    xChild: 0,
+    yChild: 0,
+    xParent: 0,
+    yParent: 0,
+    hover: false,
+    hideCursor: true,
+  },
+  computed: {
+    cursorCircle() {
+      return `transform: translateX(${this.xParent}px) translateY(${this.yParent}px) translateZ(0) translate3d(0, 0, 0);`;
+    },
+    cursorPoint() {
+      return `transform: translateX(${this.xChild - 3}px) translateY(${
+        this.yChild - 3
+      }px) translateZ(0) translate3d(0, 0, 0);`;
+    },
+  },
+  methods: {
+    moveCursor(e) {
+      this.xChild = e.clientX;
+      this.yChild = e.clientY;
+      setTimeout(() => {
+        this.xParent = e.clientX - 15;
+        this.yParent = e.clientY - 15;
+      }, 100);
+    },
+  },
+  mounted() {
+    document.addEventListener("mousemove", this.moveCursor);
+    document.addEventListener("mouseleave", (e) => {
+      this.hideCursor = true;
+    });
+    document.addEventListener("mouseenter", (e) => {
+      this.hideCursor = false;
+    });
+  },
+});
+
+var swiper = new Swiper(".mySwiper", { loop: true });
