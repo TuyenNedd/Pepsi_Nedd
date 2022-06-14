715823204b607b4a56c629ac3c353f61b7684db4
TuyenNedd <trituyen2003@gmail.com>
2022-06-14 22:43:32 +0700

    nothing, just format code

--- js/products.js
+++ js/products.js
@@ -1,88 +1,75 @@
 const hamburger = document.querySelector(
-    ".header .nav-bar .nav-list .hamburger"
-  );
-  const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
-  const menu_item = document.querySelectorAll(
-    ".header .nav-bar .nav-list ul li a"
-  );
-  const header = document.querySelector(".header.container");
-  
-  hamburger.addEventListener("click", () => {
+  ".header .nav-bar .nav-list .hamburger"
+);
+const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
+const menu_item = document.querySelectorAll(
+  ".header .nav-bar .nav-list ul li a"
+);
+const header = document.querySelector(".header.container");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  mobile_menu.classList.toggle("active");
+});
+
+document.addEventListener("scroll", () => {
+  var scroll_position = window.scrollY;
+  if (scroll_position > 10) {
+    header.style.backgroundColor = "#ffff";
+  } else {
+    header.style.backgroundColor = "transparent";
+  }
+});
+
+menu_item.forEach((item) => {
+  item.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     mobile_menu.classList.toggle("active");
   });
-  
-  document.addEventListener("scroll", () => {
-    var scroll_position = window.scrollY;
-    if (scroll_position > 10) {
-      header.style.backgroundColor = "#ffff";
-    } else {
-      header.style.backgroundColor = "transparent";
-    }
-  });
-  
-  menu_item.forEach((item) => {
-    item.addEventListener("click", () => {
-      hamburger.classList.toggle("active");
-      mobile_menu.classList.toggle("active");
+});
+
+new Vue({
+  el: "#app",
+  data: {
+    xChild: 0,
+    yChild: 0,
+    xParent: 0,
+    yParent: 0,
+    hover: false,
+    hideCursor: true,
+  },
+  computed: {
+    cursorCircle() {
+      return `transform: translateX(${this.xParent}px) translateY(${this.yParent}px) translateZ(0) translate3d(0, 0, 0);`;
+    },
+    cursorPoint() {
+      return `transform: translateX(${this.xChild - 3}px) translateY(${
+        this.yChild - 3
+      }px) translateZ(0) translate3d(0, 0, 0);`;
+    },
+  },
+  methods: {
+    moveCursor(e) {
+      this.xChild = e.clientX;
+      this.yChild = e.clientY;
+      setTimeout(() => {
+        this.xParent = e.clientX - 15;
+        this.yParent = e.clientY - 15;
+      }, 100);
+    },
+  },
+  mounted() {
+    document.addEventListener("mousemove", this.moveCursor);
+    document.addEventListener("mouseleave", (e) => {
+      this.hideCursor = true;
     });
-  });
-  
-  function changeCan(anything) {
-    document.querySelector(".pepsi_can").src = anything;
-  }
-  function changeBGBlue() {
-    document.getElementById("changeBG").style.backgroundImage =
-      "url(img/bluebg.png)";
-  }
-  function changeBGWhite() {
-    document.getElementById("changeBG").style.backgroundImage =
-      "url(img/whitebg.png)";
-  }
-  function changeBGBlack() {
-    document.getElementById("changeBG").style.backgroundImage =
-      "url(img/blackbg.png)";
-  }
-  
-  new Vue({
-    el: "#app",
-    data: {
-      xChild: 0,
-      yChild: 0,
-      xParent: 0,
-      yParent: 0,
-      hover: false,
-      hideCursor: true,
-    },
-    computed: {
-      cursorCircle() {
-        return `transform: translateX(${this.xParent}px) translateY(${this.yParent}px) translateZ(0) translate3d(0, 0, 0);`;
-      },
-      cursorPoint() {
-        return `transform: translateX(${this.xChild - 3}px) translateY(${
-          this.yChild - 3
-        }px) translateZ(0) translate3d(0, 0, 0);`;
-      },
-    },
-    methods: {
-      moveCursor(e) {
-        this.xChild = e.clientX;
-        this.yChild = e.clientY;
-        setTimeout(() => {
-          this.xParent = e.clientX - 15;
-          this.yParent = e.clientY - 15;
-        }, 100);
-      },
-    },
-    mounted() {
-      document.addEventListener("mousemove", this.moveCursor);
-      document.addEventListener("mouseleave", (e) => {
-        this.hideCursor = true;
-      });
-      document.addEventListener("mouseenter", (e) => {
-        this.hideCursor = false;
-      });
-    },
-  });
-  
-  var swiper = new Swiper(".mySwiper", { loop: true });
+    document.addEventListener("mouseenter", (e) => {
+      this.hideCursor = false;
+    });
+  },
+});
+
+VanillaTilt.init(document.querySelectorAll(".product"), {
+  max: 40,
+  speed: 5000,
+});
