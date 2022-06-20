44c543fa59897f967d90ba62a706b74305b67cfd
TuyenNedd <trituyen2003@gmail.com>
2022-06-20 17:26:15 +0700

    update Contact Page

---
+++ js/contact.js
@@ -0,0 +1,74 @@
+const hamburger = document.querySelector(
+  ".header .nav-bar .nav-list .hamburger"
+);
+const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
+const menu_item = document.querySelectorAll(
+  ".header .nav-bar .nav-list ul li a"
+);
+const header = document.querySelector(".header.container");
+
+// const stand = document.querySelector(".stand-ct");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  mobile_menu.classList.toggle("active");
+});
+
+document.addEventListener("scroll", () => {
+  var scroll_position = window.scrollY;
+  if (scroll_position > 10) {
+    header.style.backgroundColor = "#20186d";
+    stand.style.color = "#20186d";
+  } else {
+    header.style.backgroundColor = "transparent";
+    stand.style.color = "#fff";
+  }
+});
+
+menu_item.forEach((item) => {
+  item.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    mobile_menu.classList.toggle("active");
+  });
+});
+
+new Vue({
+  el: "#app",
+  data: {
+    xChild: 0,
+    yChild: 0,
+    xParent: 0,
+    yParent: 0,
+    hover: false,
+    hideCursor: true,
+  },
+  computed: {
+    cursorCircle() {
+      return `transform: translateX(${this.xParent}px) translateY(${this.yParent}px) translateZ(0) translate3d(0, 0, 0);`;
+    },
+    cursorPoint() {
+      return `transform: translateX(${this.xChild - 3}px) translateY(${
+        this.yChild - 3
+      }px) translateZ(0) translate3d(0, 0, 0);`;
+    },
+  },
+  methods: {
+    moveCursor(e) {
+      this.xChild = e.clientX;
+      this.yChild = e.clientY;
+      setTimeout(() => {
+        this.xParent = e.clientX - 15;
+        this.yParent = e.clientY - 15;
+      }, 100);
+    },
+  },
+  mounted() {
+    document.addEventListener("mousemove", this.moveCursor);
+    document.addEventListener("mouseleave", (e) => {
+      this.hideCursor = true;
+    });
+    document.addEventListener("mouseenter", (e) => {
+      this.hideCursor = false;
+    });
+  },
+});
